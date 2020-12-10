c02bd1b2af50c1739323554d20783e3ded2987a3
pyjamal <limachijonas@gmail.com>
2020-12-10 20:17:19 +0100

    pulled dev branch

--- genius-frontend/src/App.js
+++ genius-frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import Landing from "../src/pages/landing/Landing.js";
@@ -9,15 +8,6 @@ function App() {
       <div>genius brand</div>
       <Landing />
     </div>
-=======
-import logo from './logo.svg';
-//import './App.css';
-import Landing from './pages/landing'
-
-function App() {
-  return (
-    <Landing/>
->>>>>>> b5136ced40eb2e6c6dd238b802c070177d59e4fd
   );
 }
 
