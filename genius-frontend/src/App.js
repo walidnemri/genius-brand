e02b4c0bd0eab04a1ba789f9c5623cb9772ef1c5
AntoineMeunier52 <meunier.antoine52@gmail.com>
2020-12-13 20:00:56 +0100

    responsive for mobile and tablet in portrait

--- genius-frontend/src/App.js
+++ genius-frontend/src/App.js
@@ -1,11 +1,10 @@
-import Landing from './pages/landing'
-import Email from './pages/landing/EmailFormLandingPage'
+import Landing from "./pages/landing";
 
 function App() {
   return (
-    <>
-    <Landing/>
-    </>
+    <div className="App">
+      <Landing />
+    </div>
   );
 }
 
