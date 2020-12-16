e4bdba2c01b1ff4424b7e8935031a14236b35c80
pyjamal <limachijonas@gmail.com>
2020-12-16 13:50:12 +0100

    smth

--- genius-frontend/src/index.js
+++ genius-frontend/src/index.js
@@ -4,14 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Firebase, { FirebaseContext } from "./components/firebase";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <FirebaseContext.Provider value={new Firebase()}>
-      <App />
-    </FirebaseContext.Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
