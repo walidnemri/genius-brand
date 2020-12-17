8c101e51b4e517c6a36adeb591f529f53a8e355c
AntoineMeunier52 <meunier.antoine52@gmail.com>
2020-12-17 11:28:11 +0100

    add router and all page folder

--- genius-frontend/src/App.js
+++ genius-frontend/src/App.js
@@ -1,11 +1,33 @@
 import Landing from "./pages/landing";
 import Home from "./pages/home";
+import About from "./pages/about";
+import Shop from "./pages/e-shop";
+import Lookbook from "./pages/lookbook";
+import News from "./pages/news";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+        <Route path="/lookbook">
+          <Lookbook />
+        </Route>
+        <Route path="/news">
+          <News />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- genius-frontend/src/pages/home/index.js
+++ genius-frontend/src/pages/home/index.js
@@ -1,19 +1,43 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useSpring, animated } from "react-spring";
+
 const Home = () => {
   return (
     <div className="home_container">
-      <h1 className="home_brand">genius brand</h1>
-      <ul className="home_nav">
-        <li className="home_nav_ele">lookbook</li>
-        <li className="home_nav_ele">e-shop</li>
-        <li className="home_nav_ele">news</li>
-        <li className="home_nav_ele">about</li>
-      </ul>
-      <ul className="home_icon">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      <div className="home_image_background"></div>
+      <div className="home_animation_container"></div>
+      <div className="home_content_container">
+        <h1 className="home_brand">genuis brand</h1>
+        <ul className="home_nav">
+          <li className="home_nav_ele">
+            <Link className="home_link" to="/lookbook">
+              lookbook
+            </Link>
+          </li>
+          <li className="home_nav_ele">
+            <Link className="home_link" to="/shop">
+              e-shop
+            </Link>
+          </li>
+          <li className="home_nav_ele">
+            <Link className="home_link" to="/news">
+              news
+            </Link>
+          </li>
+          <li className="home_nav_ele">
+            <Link className="home_link" to="/about">
+              about
+            </Link>
+          </li>
+        </ul>
+        <ul className="home_icon">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 };
