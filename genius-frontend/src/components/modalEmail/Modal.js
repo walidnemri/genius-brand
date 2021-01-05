7d593882ccef79b7718f837cfc6e062e63cd5f3a
pyjamal <limachijonas@gmail.com>
2021-01-05 14:22:06 +0100

    some changes but not finished

---
+++ genius-frontend/src/components/modalEmail/Modal.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button onClick={handleClose}>Close</button>
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
