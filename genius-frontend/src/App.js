888755078f457ab1d1ad2141a457e628478df441
pyjamal <limachijonas@gmail.com>
2021-01-08 13:53:44 +0100

    first structure of banners, e shop, categories, etc..

--- genius-frontend/src/App.js
+++ genius-frontend/src/App.js
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
-import Landing from "../src/pages/landing/Landing.js";
-import E_shop from "../src/pages/e-shop/E-shop.js";
+import E_shop from "./pages/e-shop/E-shop.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <E_shop />
     </div>
-=======
-import Landing from './pages/landing'
-import Email from './pages/landing/EmailFormLandingPage'
-
-function App() {
-  return (
-    <>
-    <Landing/>
-    </>
->>>>>>> bd25609b2f6e34a569d83113c03f59a493581a10
   );
 }
 
--- genius-frontend/src/components/banner/Banner.js
+++ genius-frontend/src/components/banner/Banner.js
@@ -1,20 +1,29 @@
 import React from "react";
+import "./styles.css";
 
 class Banner extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="banner">
-          <p className="first">Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
-          <p>Genius Brand - </p>
+        <div className="scroll-left">
+          <p className="text-scroll">
+            - <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -
+          </p>
+          <p className="text-scroll2">
+            - <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -{" "}
+            <span id="drop">new drop in 50j : 18h : 8m : 7s</span> -{" "}
+            <span id="genius"> GENIUS brand </span> -
+          </p>
         </div>
       </div>
     );
