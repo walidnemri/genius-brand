08dcd3c8f670b55bb5885fca6f9889ad743cc65b
pyjamal <limachijonas@gmail.com>
2021-01-27 15:47:39 +0100

    finished prototype and some change  as test in component/navbar, careful to cundo the changes

--- genius-frontend/src/pages/about/index.js
+++ genius-frontend/src/pages/about/index.js
@@ -1,10 +1,138 @@
 import React from "react";
+import "./style.css";
+import NavBar from "../../components/navbar/Navbar.js";
+import part1 from "./logo/part1.png";
+import part2 from "./logo/part2.png";
+import part3 from "./logo/part3.png";
+import part4 from "./logo/part4.png";
+import Fuego from "./logo/purple-fire.jpg";
 
 function About() {
   return (
-    <div>
-      <p>about</p>
-    </div>
+    <>
+      <NavBar />
+      <div className="wrapper-body">
+        <h1>About</h1>
+        <div className="wrapper">
+          <p className="outside-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Morbi maximus eleifend quam,
+            vitae sollicitudin massa feugiat ac. Pellentesque maximus mollis
+            nisi, non eleifend urna lobortis volutpat. Donec sit amet turpis
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc at nunc imperdiet, rhoncus diam a,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Morbi maximus eleifend quam,
+            vitae sollicitudin massa feugiat ac. Pellentesque maximus mollis
+            nisi, non eleifend urna lobortis volutpat. Donec sit amet turpis
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc at nunc imperdiet, rhoncus diam a,
+            pellentesque lorem. Pellentesque mauris velit, aliquet at risus non,
+            congue eleifend risus. In vitae risus orci. Nullam a venenatis
+            magna. Morbi maximus eleifend quam, vitae sollicitudin massa feugiat
+            ac. Pellentesque maximus mollis nisi, non eleifend urna lobortis
+            volutpat. Donec sit amet turpis Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc at nunc imperdiet, rhoncus diam a,
+            pellentesque lorem. Pellentesque mauris velit, aliquet at risus non,
+            congue eleifend risus. In vitae risus orci. Nullam a venenatis
+            magna. Nullam a venenatis magna. Nullam a venenatis magna. Nullam a
+            Pellentesque mauris velit, aliquet at risus non, congue eleifend
+            Morbi maximus eleifend
+          </p>
+          <div className="top">
+            <div className="top-left">
+              <img src={part1} alt="logo" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+                nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+                mauris velit, aliquet at risus non, congue eleifend risus. In
+                vitae risus orci. Nullam a venenatis magna. Morbi maximus
+                eleifend quam, vitae sollicitudin massa feugiat ac. Pellentesque
+                maximus mollis nisi, non
+              </p>
+            </div>
+            <div className="top-right">
+              <img src={part2} alt="logo" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+                nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+                mauris velit, aliquet at risus non, congue eleifend risus. In
+                vitae risus orci. Nullam a venenatis magna. Morbi maximus
+                eleifend quam, vitae sollicitudin massa feugiat ac. Pellentesque
+                maximus mollis nisi, non
+              </p>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="bottom-left">
+              <img src={part3} alt="logo" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+                nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+                mauris velit, aliquet at risus non, congue eleifend risus. In
+                vitae risus orci. Nullam a venenatis magna. Morbi maximus
+                eleifend quam, vitae sollicitudin massa feugiat ac. Pellentesque
+                maximus mollis nisi, non congue eleifend risus mollis
+              </p>
+            </div>
+            <div className="bottom-right">
+              <img src={part4} alt="logo" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+                nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+                mauris velit, aliquet at risus non, congue eleifend risus. In
+                vitae risus orci. Nullam a venenatis magna. Morbi maximus
+                eleifend quam, vitae sollicitudin massa feugiat ac. Pellentesque
+                maximus mollis nisi, non congue eleifend risus mollis
+              </p>
+            </div>
+          </div>
+          <p className="outside-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Morbi maximus eleifend quam,
+            vitae sollicitudin massa feugiat ac. Pellentesque maximus mollis
+            nisi, non eleifend urna lobortis volutpat. Donec sit amet turpis
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            nunc imperdiet, rhoncus diam a, pellentesque lorem. Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend risus. In vitae
+            risus orci. Nullam a venenatis magna. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc at nunc imperdiet, rhoncus diam a,
+            pellentesque lorem. Pellentesque mauris velit, aliquet at risus non,
+            congue eleifend risus. In vitae risus orci. Nullam a venenatis
+            magna. Morbi maximus eleifend quam, vitae sollicitudin massa feugiat
+            ac. Pellentesque maximus mollis nisi, non eleifend urna lobortis
+            volutpat. Donec sit amet turpis Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc at nunc imperdiet, rhoncus diam a,
+            pellentesque lorem. Pellentesque mauris velit, aliquet at risus non,
+            congue eleifend risus. In vitae risus orci. Nullam a venenatis
+            magna. Nullam a venenatis magna. Nullam a venenatis magna. Nullam a
+            Pellentesque mauris velit, aliquet at risus non, congue eleifend
+            risus. In vitae risus orci. Nullam a venenatis magna. Morbi maximus
+            eleifend quam, vitae sollicitudin massa feugiat ac. Pellentesque
+            maximus mollis nisi, non eleifend urna lobortis volutpat. Donec sit
+            amet turpis Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc at nunc imperdiet, rhoncus diam a, pellentesque lorem.
+            Pellentesque mauris velit, aliquet at risus non, congue eleifend
+            risus. In vitae risus orci. Nullam a venenatis magna. Nullam a
+            venenatis magna. Nullam a venenatis magna. Nullam a Pellentesque
+            mauris velit, aliquet at risus non, congue eleifend Morbi maximus
+            eleifend quam, vitae a risus toti
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
