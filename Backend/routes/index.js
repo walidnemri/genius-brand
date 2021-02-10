604cbc5b4f2d572f03ac87e68e90f70ccfa45dfe
walidnemri <nemriwalid@hotmail.com>
2021-02-10 16:27:46 +0100

    category-route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router()
 const productsRoute = require('./products.routes')
+const categoriesRoute = require('./categories.routes')
 
 router.use('/products', productsRoute)
+router.use('/categories', categoriesRoute)
 
 module.exports = router
